a84ea3b1d8d3ed69c4d6e925caddfaf7ae4be2ba
Sarah <sarah.breen@hotmail.com>
2022-09-26 13:21:05 +0100

    Offical Password Manager code

---
+++ password_manager.rb
@@ -0,0 +1,14 @@
+class PasswordManager
+    
+    def add(service, password)
+        #?
+    end
+    #takes one string as argument
+    def password_for(service)
+        #?
+    end 
+    #takes no arguments 
+    def services
+        #?
+    end 
+end 
